7492388ae81e132f77f05ad8df234761afa5bdcc
MartinVillegas25 <martinvillegas90@hotmail.com>
2023-10-27 10:02:57 -0300

    portdb

--- database.js
+++ database.js
@@ -8,7 +8,8 @@ const config = {
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 }
 
 const pool = mysql.createPool(config, ()=>{
